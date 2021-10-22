c28c389a434496ccabe65b9df4f1833601ec87b0
Manasi Patil <36513491+itsmnsi@users.noreply.github.com>
2021-10-22 14:59:58 +0530

    Create sample.sql

---
+++ RDBMS/Data Definition Language/Create Mobile_specification table/sample.sql
@@ -0,0 +1,17 @@
+CREATE TABLE Mobile_Specification(
+  Spec_No varchar(10) PRIMARY KEY,
+  Dimension varchar(20),
+  Weight varchar(20),
+  Display_Type varchar(20),
+  Display_Size varchar(20),
+  Internal_mem_in_MB number(10),
+  Memory_Card_Capacity_GB number(10),
+  Network_3G varchar(5),
+  GPRS varchar(5),
+  EDGE varchar(5),
+  Bluetooth varchar(5),
+  Camera varchar(5),
+  Camera_Quality varchar(5),
+  OS varchar(20),
+  Battery_Life_Hrs number(10)
+);
