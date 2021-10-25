9bebdf003d1e6e18c4140050ed523b5be096b864
Manasi Patil <36513491+itsmnsi@users.noreply.github.com>
2021-10-25 11:26:27 +0530

    Create model.sql

---
+++ RDBMS/Data Manipulation Language/Insert - All records customer_info table/model.sql
@@ -0,0 +1,2 @@
+insert into Customer_Info(Customer_ID, Customer_Name, Address, Mobile, Email) values('C01','Amal','Delhi',1234564566,'amal@gmail.com');
+insert into Customer_Info(Customer_ID, Customer_Name, Address, Mobile, Email) values('C02','Ajay','Bangalore',1234565689,'ajay@yahoo.com');
