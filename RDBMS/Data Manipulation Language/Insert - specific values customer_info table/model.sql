a4b226099bf5fce80f85702c4708fd417e3aa394
Manasi Patil <36513491+itsmnsi@users.noreply.github.com>
2021-10-25 11:40:00 +0530

    Create model.sql

---
+++ RDBMS/Data Manipulation Language/Insert - specific values customer_info table/model.sql
@@ -0,0 +1,5 @@
+insert into Customer_Info(Customer_ID, Customer_Name, Address, Email) values('C01','Mary','Delhi','mary@gmail.com');
+insert into Customer_Info(Customer_ID, Customer_Name, Address, Email) values('C02','Hema','Bangalore','hema@yahoo.com');
+insert into Customer_Info(Customer_ID, Customer_Name, Address, Email) values('C03','Rocky','Chennai','rocky12@gmail.com');
+insert into Customer_Info(Customer_ID, Customer_Name, Address, Email) values('C04','Robin','Mumbai','robbywood@yahoo.co.in');
+insert into Customer_Info(Customer_ID, Customer_Name, Address, Email) values('C05','Swetha','Hyderabad','Swethu1993@gmail.com');
