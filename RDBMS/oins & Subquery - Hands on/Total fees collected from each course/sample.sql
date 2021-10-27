9fe890f5e43de2159de1eab55b96379acb141667
Manasi Patil <pmanasi02@gmail.com>
2021-10-27 12:19:00 +0530

    Delete sample.sql

--- RDBMS/oins & Subquery - Hands on/Total fees collected from each course/sample.sql
+++
@@ -1,4 +0,0 @@
-select course.courseid, CourseName, sum(Fees) as TOTALFEES from COURSE
-inner join REGISTRATION on course.courseid=registration.CourseID
-group by course.courseid, CourseName
-order by course.courseid;
