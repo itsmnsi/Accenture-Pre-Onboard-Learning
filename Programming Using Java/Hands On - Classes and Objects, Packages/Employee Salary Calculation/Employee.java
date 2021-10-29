b9be4c819541a25a3b017af0c24bab8eac69005b
Manasi Patil <pmanasi02@gmail.com>
2021-10-29 12:42:20 +0530

    Create Employee.java

---
+++ Programming Using Java/Hands On - Classes and Objects, Packages/Employee Salary Calculation/Employee.java
@@ -0,0 +1,45 @@
+public class Employee
+{
+    private int employeeId;
+    private String employeeName;
+    private double salary;
+    private double netSalary;
+    public int getEmployeeId()
+    {
+        return employeeId;
+    }
+    public String getEmployeeName()
+    {
+        return employeeName;
+    }
+    public double getSalary()
+    {
+        return salary;
+    }
+    public double getNetSalary()
+    {
+        return netSalary;
+    }
+    public void setEmployeeId(int employeeId)
+    {
+        this.employeeId = employeeId;
+    }
+    public void setEmployeeName(String employeeName)
+    {
+        this.employeeName = employeeName;
+    }
+    public void setSalary(double salary)
+    {
+        this.salary = salary;
+    }
+    public void setNetSalary(double netSalary)
+    {
+        this.netSalary = netSalary;
+    }
+    public void calculateNetSalary(int pfpercentage)
+    {
+        double pfamount;
+        pfamount = salary * pfpercentage/100;
+        netSalary = salary - pfamount;
+    }
+}
