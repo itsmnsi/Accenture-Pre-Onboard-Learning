2ea0b96b2b16826b35035e3417105d3e108fcf2c
Manasi Patil <pmanasi02@gmail.com>
2021-11-01 09:02:30 +0530

    Create Person.java

---
+++ Programming Using Java/Hands On - Classes and Objects, Packages/Travel Details/Person.java
@@ -0,0 +1,30 @@
+public class Person 
+{
+    private String name;
+    private char gender;
+    private int age;
+    public String getName()
+    {
+        return name;
+    }
+    public char getGender()
+    {
+        return gender;
+    }
+    public int getAge()
+    {
+        return age;
+    }
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+    public void setGender(char gender)
+    {
+        this.gender = gender;
+    }
+    public void setAge(int age)
+    {
+        this.age = age;
+    }
+}
